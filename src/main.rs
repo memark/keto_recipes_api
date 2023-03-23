8a5d3d79c1bb9ad55b5f28915bfb7153aaf5cbf4
Magnus Markling <magnus@markling.com>
2023-03-23 20:49:02 +0100

    add data compression

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod data;
 use data::*;
 
 use axum::{ routing::get, http::StatusCode, Json, Router };
+use tower_http::compression::CompressionLayer;
 use std::net::SocketAddr;
 use tower_http::cors::{ CorsLayer, Any };
 use tower_http::trace::TraceLayer;
@@ -24,6 +25,7 @@ fn create_router() -> Router {
         .route("/", get(root))
         .route("/recipes", get(get_recipes))
         .layer(CorsLayer::new().allow_origin(Any))
+        .layer(CompressionLayer::new())
         .layer(TraceLayer::new_for_http())
 }
 
@@ -51,7 +53,7 @@ async fn get_recipes() -> (StatusCode, Json<Vec<Recipe>>) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use axum::{ body::Body, http::{ Request, StatusCode } };
+    use axum::{ body::Body, http::{ Request, StatusCode, HeaderValue } };
     use pretty_assertions::assert_eq;
     use tower::ServiceExt;
 
@@ -86,5 +88,28 @@ mod tests {
             .unwrap();
 
         assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn should_compress_data_correctly() {
+        let response = create_router()
+            .clone()
+            .oneshot(Request::builder().uri("/recipes").body(Body::empty()).unwrap()).await
+            .unwrap();
+        assert_eq!(response.headers().contains_key("content-encoding"), false);
+
+        let response = create_router()
+            .oneshot(
+                Request::builder()
+                    .uri("/recipes")
+                    .header("accept-encoding", "gzip")
+                    .body(Body::empty())
+                    .unwrap()
+            ).await
+            .unwrap();
+        assert_eq!(
+            response.headers().get("content-encoding").unwrap_or(&HeaderValue::from_static("")),
+            "gzip"
+        );
     }
 }
